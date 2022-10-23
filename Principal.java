5254c55230e7aed2fc7307e3170a1b43d6e3b6de
Simon Villa Escobar <116121749+Moncho02@users.noreply.github.com>
2022-10-23 17:43:03 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,149 @@
+/* Fundamentos de programación
+ * Proyecto 1
+ * Juan Diego Tamayo Vergara, Jacobo Espinosa Blandón y Simon Villa Escobar
+ */
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("A continuación lea las siguientes instrucciones: ");
+        System.out.println("0 detiene el programa.");
+        System.out.println("1 para ingresar un vehículo.");
+        System.out.println("2 muestra la información almacenada de los vehículos.");
+        System.out.println("3 cantidad de vehículos almacenados.");
+        System.out.println("4 muestra la información de vehículos con color verde.");
+        System.out.println("5 muestra la información de vehículos con modelo entre el año 2001 y 2021.");
+        System.out.println("6 para ingresar un sensor.");
+        System.out.println("7 muestra la información almacenada de los sensores.");
+        System.out.println("8 cantidad de sensores almacenados.");
+        System.out.println("9 muestra la información de sensores tipo temperatura.");
+        System.out.println("666 muestra la información de sensores tipo temperatura y ordenados por valor comercial.");
+        System.out.println("");
+
+        mostrarMenu();
+    }
+
+    private static void mostrarMenu() {
+
+        Vehiculo vehiculo = new Vehiculo();
+        int modelo = 0;
+        String marca = "N/A";
+        double valorComercial = 0;
+        Vehiculo siguienteVehiculo = null;
+
+        Sensor sensor = new Sensor();
+        String tipo = "N/A";
+
+        @SuppressWarnings("resource")
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Ingrese el número para su necesidad: ");
+
+        boolean continua = true;
+
+        int decision = lectura.nextInt();
+
+        if (decision == 0) {
+            System.out.println("Lástima que no nos probaste. Te esperamos en una próxima ocasión!");
+            continua = false;
+        }
+
+        while (continua) {
+
+            if (decision == 1) {
+                if (vehiculo.getTamaño() > vehiculo.getPosAnadir()) {
+                    System.out.println("Ingrese el modelo: ");
+                    modelo = lectura.nextInt();
+
+                    System.out.println("Ingrese el marca: ");
+                    marca = lectura.next();
+
+                    System.out.println("Ingrese el valor comercial: ");
+                    valorComercial = lectura.nextDouble();
+
+                    siguienteVehiculo = new Vehiculo(modelo, marca, valorComercial);
+
+                    System.out.println("Desea ingresar el color de vehículo? S/N ");
+
+                    if (lectura.next().toUpperCase().equalsIgnoreCase("S")) {
+                        System.out.println("Ingrese el color de vehículo: ");
+                        siguienteVehiculo = new Vehiculo(modelo, marca, valorComercial, lectura.next());
+                    }
+
+                    vehiculo.getVehiculos()[vehiculo.getPosAnadir()] = siguienteVehiculo;
+                    vehiculo.setPosAnadir(1);
+                } else {
+                    System.out.println("Error base de datos de vehículos llena.");
+                }
+            }
+
+            if (decision == 0) {
+                System.out.println("Gracias por utilizarnos. Te esperamos de nuevo!");
+                continua = false;
+            }
+
+            if (decision == 2) {
+                System.out.println(vehiculo.toStringVehiculos());
+            }
+
+            if (decision == 3) {
+                System.out.println("Cantidad de vehículos almacenados: " + vehiculo.cantidadVehiculos() + ".");
+            }
+
+            // por desarrollar
+            if (decision == 4) {
+                System.out.println(vehiculo.obtenerVehiculosVerdes(vehiculo));
+            }
+
+            if (decision == 5) {
+                System.out.println(vehiculo.obtenerVehiculosPorModelo(vehiculo));
+            }
+
+            if (decision == 6) {
+                if (sensor.getTamaño() > sensor.getPosAnadir()) {
+
+                    System.out.println("Ingrese el tipo: ");
+                    tipo = lectura.next();
+
+                    System.out.println("Ingrese el valor comercial: ");
+                    valorComercial = lectura.nextDouble();
+
+                    sensor.getSensores()[sensor.getPosAnadir()] = new Sensor(tipo, valorComercial);
+                    sensor.setPosAnadir(1);
+                } else {
+                    System.out.println("Error base de datos de sensores llena.");
+                }
+
+                if (tipo.equalsIgnoreCase("temperatura")) {
+                    sensor.setCantidadSensoresTemperatura(1);
+                }
+            }
+
+            if (decision == 7) {
+                System.out.println(sensor.toStringSensores());
+            }
+
+            if (decision == 8) {
+                System.out.println(sensor.cantidadSensores());
+            }
+
+            if (decision == 9) {
+                System.out.println(sensor.getSensoresTemperatura(sensor).toStringSensores());
+            }
+
+            if (decision == 666) {
+                Sensor sensorParam = sensor.getSensoresTemperatura(sensor);
+                System.out.println(sensor.getSensoresTemperaturaSort(sensorParam));
+            }
+
+            System.out.println("Que desea hacer? ");
+            decision = lectura.nextInt();
+
+        }
+
+    }
+
+}
---
+++ Vehiculo.java
@@ -0,0 +1,152 @@
+public class Vehiculo {
+
+    private int tamaño = 10;
+    private Vehiculo[] vehiculos = new Vehiculo[tamaño];
+    private int posAnadir = 0;
+    private int modelo;
+    private String marca;
+    private double valorComercial;
+    private String color;
+
+    public Vehiculo() {
+        super();
+
+        // TODO Auto-generated constructor stub
+    }
+
+    public Vehiculo(int modelo, String marca, double valorComercial) {
+        this(modelo, marca, valorComercial, "verde");
+    }
+
+    public Vehiculo(int modelo, String marca, double valorComercial, String color) {
+        super();
+        this.modelo = modelo;
+        this.marca = marca;
+        this.valorComercial = valorComercial;
+        this.color = color;
+    }
+
+    public int cantidadVehiculos() {
+        return this.posAnadir;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public int getPosAnadir() {
+        return posAnadir;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public double getValorComercial() {
+        return valorComercial;
+    }
+
+    public Vehiculo[] getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPosAnadir(int posAnadir) {
+        this.posAnadir = this.posAnadir + posAnadir;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public void setValorComercial(double valorComercial) {
+        this.valorComercial = valorComercial;
+    }
+
+    public void setVehiculos(Vehiculo[] vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [modelo=" + modelo + ", marca=" + marca + ", valorComercial=" + valorComercial + ", color="
+                + color + "]";
+    }
+
+    public String toStringVehiculos() {
+        String informacionTotal = "Información de todos los vehículos almacenados hasta el momento." + "\n" + "\n";
+        int posicionVehiculo = 1;
+        for (int i = 0; i < this.posAnadir; i++) {
+            informacionTotal = informacionTotal + posicionVehiculo + " | " + this.vehiculos[i].toString() + "\n";
+            ++posicionVehiculo;
+        }
+
+        return informacionTotal;
+    }
+
+    public String obtenerVehiculosVerdes(Vehiculo vehiculo) {
+        int posicionVehiculo = 1;
+        int cantidadVehiculosVerdes = 0;
+        String vehiculosVerdes = "Vehículos registrados con color verde. \n \n";
+        if (vehiculo.cantidadVehiculos() > 0) {
+            for (int i = 0; i < vehiculo.cantidadVehiculos(); i++) {
+                if (vehiculo.getVehiculos()[i].getColor().toUpperCase().equalsIgnoreCase("verde")) {
+                    vehiculosVerdes = vehiculosVerdes + posicionVehiculo + " | " + vehiculo.getVehiculos()[i].toString()
+                            + "\n";
+                    ++posicionVehiculo;
+                    ++cantidadVehiculosVerdes;
+                }
+            }
+        }
+
+        if (cantidadVehiculosVerdes == 0) {
+            vehiculosVerdes = vehiculosVerdes + cantidadVehiculosVerdes;
+        }
+
+        return vehiculosVerdes;
+    }
+
+    public String obtenerVehiculosPorModelo(Vehiculo vehiculo) {
+        int posicionVehiculo = 1;
+        int cantidadVehiculosModelo = 0;
+        int modelo = 0;
+        String vehiculosModelo = "Vehículos registrados con modelo entre 2000 y 2021. \n \n";
+        if (vehiculo.cantidadVehiculos() > 0) {
+            for (int i = 0; i < vehiculo.cantidadVehiculos(); i++) {
+                modelo = vehiculo.getVehiculos()[i].getModelo();
+                if (modelo >= 2000 && modelo <= 2021) {
+                    vehiculosModelo = vehiculosModelo + posicionVehiculo + " | " + vehiculo.getVehiculos()[i].toString()
+                            + "\n";
+                    ++posicionVehiculo;
+                    ++cantidadVehiculosModelo;
+                }
+            }
+        }
+
+        if (cantidadVehiculosModelo == 0) {
+            vehiculosModelo = vehiculosModelo + cantidadVehiculosModelo;
+        }
+
+        return vehiculosModelo;
+    }
+
+}
